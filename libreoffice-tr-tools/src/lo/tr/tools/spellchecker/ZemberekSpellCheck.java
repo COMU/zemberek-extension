640b3c2f989eec60362104406c9e1fc84ff265f2
talhakanyilmaz <talhakanyilmaz18@gmail.com>
2018-10-30 17:02:15 +0300

    Limit Suggestion Count

--- libreoffice-tr-tools/src/lo/tr/tools/spellchecker/ZemberekSpellCheck.java
+++ libreoffice-tr-tools/src/lo/tr/tools/spellchecker/ZemberekSpellCheck.java
@@ -1,36 +1,42 @@
 package lo.tr.tools.spellchecker;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
 import zemberek.morphology.TurkishMorphology;
 import zemberek.normalization.TurkishSpellChecker;
 
 public class ZemberekSpellCheck implements TurkishLinguist {
 
-  public static ZemberekSpellCheck instance = new ZemberekSpellCheck();
+    public static ZemberekSpellCheck instance = new ZemberekSpellCheck();
 
-  private TurkishMorphology morphology;
-  private TurkishSpellChecker spellChecker;
+    private TurkishMorphology morphology;
+    private TurkishSpellChecker spellChecker;
 
 
-  private ZemberekSpellCheck() {
-    this.morphology = TurkishMorphology.createWithDefaults();
-    try {
-      this.spellChecker = new TurkishSpellChecker(morphology);
-    } catch (IOException e) {
-      e.printStackTrace();
+    private ZemberekSpellCheck() {
+        this.morphology = TurkishMorphology.createWithDefaults();
+        try {
+            this.spellChecker = new TurkishSpellChecker(morphology);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  public static ZemberekSpellCheck getInstance() {
-    return instance;
-  }
+    public static ZemberekSpellCheck getInstance() {
+        return instance;
+    }
 
-  public boolean isCorrect(String w) {
-    return spellChecker.check(w);
-  }
+    public boolean isCorrect(String w) {
+        return spellChecker.check(w);
+    }
 
-  public List<String> getSuggestions(String s) {
-    return spellChecker.suggestForWord(s);
-  }
+    public List<String> getSuggestions(String s) {
+        List<String> suggestions = spellChecker.suggestForWord(s);
+        if (suggestions.size() > 7)
+            return suggestions.subList(0, 7);
+        else
+            return suggestions;
+    }
 }
