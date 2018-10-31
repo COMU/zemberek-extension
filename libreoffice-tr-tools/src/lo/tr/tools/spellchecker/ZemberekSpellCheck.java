7cc98a7fe734987ed41bde62e3f7100756705a78
talhakanyilmaz <talhakanyilmaz18@gmail.com>
2018-11-01 01:01:39 +0300

    Extract removePunctuation method. Change Code Styles.

--- libreoffice-tr-tools/src/lo/tr/tools/spellchecker/ZemberekSpellCheck.java
+++ libreoffice-tr-tools/src/lo/tr/tools/spellchecker/ZemberekSpellCheck.java
@@ -9,34 +9,39 @@ import zemberek.normalization.TurkishSpellChecker;
 
 public class ZemberekSpellCheck implements TurkishLinguist {
 
-    public static ZemberekSpellCheck instance = new ZemberekSpellCheck();
+  public static ZemberekSpellCheck instance = new ZemberekSpellCheck();
 
-    private TurkishMorphology morphology;
-    private TurkishSpellChecker spellChecker;
+  private TurkishMorphology morphology;
+  private TurkishSpellChecker spellChecker;
 
 
-    private ZemberekSpellCheck() {
-        this.morphology = TurkishMorphology.createWithDefaults();
-        try {
-            this.spellChecker = new TurkishSpellChecker(morphology);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+  private ZemberekSpellCheck() {
+    this.morphology = TurkishMorphology.createWithDefaults();
+    try {
+      this.spellChecker = new TurkishSpellChecker(morphology);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    public static ZemberekSpellCheck getInstance() {
-        return instance;
-    }
+  public static ZemberekSpellCheck getInstance() {
+    return instance;
+  }
 
-    public boolean isCorrect(String w) {
-        return spellChecker.check(w.replaceAll("\\p{Punct}+$", ""));
-    }
+  public boolean isCorrect(String w) {
+    return spellChecker.check(removePunctuation(w));
+  }
 
-    public List<String> getSuggestions(String s) {
-        List<String> suggestions = spellChecker.suggestForWord(s.replaceAll("\\p{Punct}+$", ""));
-        if (suggestions.size() > 7)
-            return suggestions.subList(0, 7);
-        else
-            return suggestions;
+  public List<String> getSuggestions(String s) {
+    List<String> suggestions = spellChecker.suggestForWord(removePunctuation(s));
+    if (suggestions.size() > 7) {
+      return suggestions.subList(0, 7);
     }
+    return suggestions;
+  }
+
+  private String removePunctuation(String s) {
+    return s.replaceAll("\\p{Punct}+$", "");
+  }
+
 }
